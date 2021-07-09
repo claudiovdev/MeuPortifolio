fc0e1151d92f9bbd4538ffaffd89419b7c04cb07
viniciuscfn <claudio.vdev@gmail.com>
2021-07-08 23:02:11 -0300

    Criando template de components

--- Meu-Portifolio/src/app/app.module.ts
+++ Meu-Portifolio/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { BodyComponent } from './components/template/body/body.component';
+import { FooterComponent } from './components/template/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
